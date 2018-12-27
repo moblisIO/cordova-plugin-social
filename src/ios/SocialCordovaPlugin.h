3e7c0dd465ed3d51d74275fb805bbf43cf1fc746
moblisIO <45700017+moblisIO@users.noreply.github.com>
2018-12-26 19:01:43 -0500

    Delete SocialCordovaPlugin.h

--- src/ios/SocialCordovaPlugin.h
+++
@@ -1,9 +0,0 @@
-#import <Cordova/CDVPlugin.h>
-
-@interface SocialCordovaPlugin : CDVPlugin <UIPopoverControllerDelegate>
-
-// The hooks for our plugin commands
-- (void)share:(CDVInvokedUrlCommand *)command;
-
-
-@end
